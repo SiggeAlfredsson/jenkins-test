63062c5e70caa5a5b1f813cd8e93d22257182701
Sigge <saorust@hotmail.com>
2023-09-13 11:14:26 +0200

    adding bare project

---
+++ src/main/java/com/example/calcjenkinsdemo/CalcJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.calcjenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcJenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcJenkinsDemoApplication.class, args);
+    }
+
+}
